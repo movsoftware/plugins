b645ac8c85138d9000fd6d6c798910d225070157
pendleto <pendleto@movsoftware.com>
2021-01-17 19:21:32 -0500

    Add index

--- net/yaf/src/opnsense/mvc/app/controllers/OPNsense/Yaf/GeneralController.php
+++ net/yaf/src/opnsense/mvc/app/controllers/OPNsense/Yaf/GeneralController.php
@@ -7,6 +7,6 @@ class GeneralController extends \OPNsense\Base\IndexController
     public function indexAction()
     {
         $this->view->generalForm = $this->getForm("general");
-        $this->view->pick('OPNsense/Yaf/general');
+        $this->view->pick('OPNsense/Yaf/index');
     }
 }
